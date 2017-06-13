caf9c96dae78aa88289b998ca946a15753316627
j8298c <juliocmojica@gmail.com>
2017-06-13 16:41:35 -0400

    added descriptions and buttons for projects

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
+//menu toggle in mobile view
 (function (window, document) {
       document.getElementById('toggle').addEventListener('click', function (e) {
           document.getElementById('tuckedMenu').classList.toggle('custom-menu-tucked');
           document.getElementById('toggle').classList.toggle('x');
       });
       })(this, this.document);
+
+      //adds tech buttons to page takes in an array
+      function addTech(arr, projectDiv){
+        const $projectTech = projectDiv;
+        arr.forEach((button)=>{
+          $projectTech.append('<button class="pure-button pure-button-primary techbtn">' + button + '</button>' );
+        })
+      };
+
+      //decalre projects and run addtech function
+      const $apollo = $('.apollo')
+      const apollotech = ['node', 'mongo', 'mongoose', 'react', 'redux']
+      addTech(apollotech, $apollo);
+
+      const $githubCard = $('.github-card');
+      const githubtech = ['Yarn', 'Webpack', 'Chai', 'Enzyme', 'React', 'React-Router', 'Redux', 'Mocha'];
+      addTech(githubtech, $githubCard);
+
+      const $yumshub = $('.yumshub');
+      const yumshubtech = []
