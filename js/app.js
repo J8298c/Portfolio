7719b7eb144af2811f36bcd5bd52ce221db7eb6a
j8298c <juliocmojica@gmail.com>
2017-06-17 10:52:33 -0400

    add about me

--- js/app.js
+++
@@ -1,9 +0,0 @@
-//function to control the bar animation when in mobile view
-(function (window, document) {
-            document.getElementById('toggle').addEventListener('click', function (e) {
-                document.getElementById('tuckedMenu').classList.toggle('custom-menu-tucked');
-                document.getElementById('toggle').classList.toggle('x');
-            });
-        })(this, this.document);
-
-
